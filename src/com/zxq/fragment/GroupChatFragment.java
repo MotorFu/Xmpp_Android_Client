a2bc717cf660a6f9dc3d2963286ba3d1d84e72cb
zxq <853294317@qq.com>
2014-09-14 00:30:24 +0800

    做好群聊天伪数据展示部分

--- src/com/zxq/fragment/GroupChatFragment.java
+++ src/com/zxq/fragment/GroupChatFragment.java
@@ -1,5 +1,6 @@
 package com.zxq.fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -14,6 +15,8 @@ import com.zxq.service.XmppService;
 import com.zxq.xmpp.R;
 
 import java.awt.font.TextAttribute;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -23,13 +26,14 @@ public class GroupChatFragment extends Fragment {
     private ListView listView;
     private XmppService mXmppService;
     private static GroupChatFragment groupChatFragment;
+    private GroupChatAdapter groupChatAdapter;
 
     private GroupChatFragment(XmppService mXmppService) {
         this.mXmppService = mXmppService;
     }
 
-    public static GroupChatFragment getInstance(XmppService mXmppService){
-        if(groupChatFragment == null)
+    public static GroupChatFragment getInstance(XmppService mXmppService) {
+        if (groupChatFragment == null)
             groupChatFragment = new GroupChatFragment(mXmppService);
         return groupChatFragment;
     }
@@ -44,11 +48,20 @@ public class GroupChatFragment extends Fragment {
     }
 
 
-
     private void initView(View inflate) {
         listView = (ListView) inflate.findViewById(R.id.group_chat_tree_view);
     }
+
     private void setupData() {
+        ArrayList<GroupEntry> groupEntries = new ArrayList<GroupEntry>();
+        groupEntries.add(new GroupEntry("", "这是第一个群"));
+        groupEntries.add(new GroupEntry("", "这是第二个群"));
+        groupEntries.add(new GroupEntry("", "这是第三个群"));
+        groupEntries.add(new GroupEntry("", "这是第四个群"));
+        groupEntries.add(new GroupEntry("", "这是第五个群"));
+        groupEntries.add(new GroupEntry("", "这是第六个群"));
+        groupChatAdapter = new GroupChatAdapter(groupEntries,this.getActivity());
+        listView.setAdapter(groupChatAdapter);
     }
 
     @Override
@@ -67,46 +80,93 @@ public class GroupChatFragment extends Fragment {
     }
 
     private class GroupChatAdapter extends BaseAdapter {
-      //  List<>
+        private List<GroupEntry> groupList;
+        private Context context;
+
+        private GroupChatAdapter() {
+        }
+
+        public List<GroupEntry> getGroupList() {
+            return groupList;
+        }
+
+        public void setGroupList(List<GroupEntry> groupList) {
+
+            this.groupList = groupList;
+        }
+
+        private GroupChatAdapter(List<GroupEntry> groupList, Context context) {
+            this.groupList = groupList;
+            this.context = context;
+        }
 
         @Override
         public int getCount() {
-            return 0;
+            return groupList.size();
         }
 
         @Override
         public Object getItem(int position) {
-            return null;
+            return groupList.get(position);
         }
 
         @Override
         public long getItemId(int position) {
-            return 0;
+            return position;
         }
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            return null;
+            GroupHolder groupHolder;
+            GroupEntry groupEntry = groupList.get(position);
+            if (convertView == null) {
+                groupHolder = new GroupHolder();
+                convertView = LayoutInflater.from(context).inflate(R.layout.fragment_group_chat_tree_item, null);
+                groupHolder.groupIcon = (ImageView) convertView.findViewById(R.id.group_chat_item_icon);
+                groupHolder.groupTitle = (TextView) convertView.findViewById(R.id.group_chat_item_title);
+                convertView.setTag(groupHolder);
+            } else {
+                groupHolder = (GroupHolder) convertView.getTag();
+            }
+            groupHolder.groupTitle.setText(groupEntry.getTitle());
+
+            return convertView;
+        }
+
+        private class GroupHolder {
+            public ImageView groupIcon;
+            public TextView groupTitle;
         }
     }
-    private class Group{
-        private ImageView groupIcon;
-        private TextView groupTitle;
 
-        public void setGroupIcon(ImageView groupIcon) {
-            this.groupIcon = groupIcon;
+    private class GroupEntry {
+        private String iconUrl;
+        private String title;
+
+        private GroupEntry() {
         }
 
-        public void setGroupTitle(TextView groupTitle) {
-            this.groupTitle = groupTitle;
+        private GroupEntry(String iconUrl, String title) {
+            this.iconUrl = iconUrl;
+            this.title = title;
         }
 
-        public ImageView getGroupIcon() {
-            return groupIcon;
+        public void setIconUrl(String iconUrl) {
+            this.iconUrl = iconUrl;
         }
 
-        public TextView getGroupTitle() {
-            return groupTitle;
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public String getIconUrl() {
+
+            return iconUrl;
+        }
+
+        public String getTitle() {
+            return title;
         }
     }
+
 }
